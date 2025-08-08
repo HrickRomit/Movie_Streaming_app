269819a9cb6701c8e1a045befe35cce5f17c85a7
zoshikiyaman <yoshikizbd15@gmail.com>
2025-08-08 19:31:04 +0600

    nga
    nga

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,9 @@
 
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="../pages/index.php">MovieDB</a>
+  <a class="navbar-brand" href="../pages/index.php">
+    <img src="../uploads/logos/main_logo.jpg" alt="MovieDB Logo" height="40">
+</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
